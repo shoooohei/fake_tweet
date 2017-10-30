629f8c09646a4b410eb3187a17e5405d5a7465ac
Shohei <shoheimoment@gmail.com>
2017-10-30 14:57:11 +0900

    s

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery/dist/jquery.js
+//= require bootstrap/dist/js/bootstrap.js
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
