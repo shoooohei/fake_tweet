785b880638835cd98def7313e771c011b3dc54d5
Shohei <shoheimoment@gmail.com>
2017-11-11 17:09:18 +0900

    complete

--- app/controllers/faketweets_controller.rb
+++ app/controllers/faketweets_controller.rb
@@ -1,15 +1,15 @@
 class FaketweetsController < ApplicationController
   before_action :set_tweet, only: [:edit, :update, :destroy]
-  
+
   def index
     @alltweet = Faketweet.order(created_at: :desc)
-    # @faketweet = 
+    # @faketweet =
   end
-  
+
   def new
     @faketweet = Faketweet.new
   end
-  
+
   def create
      @faketweet = Faketweet.new(tweet_params)
      if @faketweet.save
@@ -19,10 +19,10 @@ class FaketweetsController < ApplicationController
       render "index"
      end
   end
-  
+
   def edit
   end
-  
+
   def update
     if @faketweet.update(tweet_params)
       redirect_to faketweets_path, notice: "更新できました"
@@ -30,18 +30,18 @@ class FaketweetsController < ApplicationController
       render "edit"
     end
   end
-  
+
   def destroy
     @faketweet.destroy
     redirect_to faketweets_path, notice: "削除できました"
   end
-    
-  
+
+
   private
   def tweet_params
     params.require(:faketweet).permit(:content)
   end
-  
+
   def set_tweet
     @faketweet = Faketweet.find(params[:id])
   end
